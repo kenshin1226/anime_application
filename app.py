a597d12b3f90a752a0d3c6cade70837daf982f70
kenshin1226 <kenshin.sugizaki@gmail.com>
2026-05-24 16:23:28 +0900

    first

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,session, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/list')
+def list():
+        conn=sqlite3.connect('anime.db')
+        cur=conn.cursor()
+        cur.execute('select * from anime')
+        anime_list=cur.fetchall()
+        print("@12",anime_list)
+        return render_template('list.html',a_list=anime_list)
+@app.route('/input')
+def input():
+      return render_template('input.html')
+@app.route('/input2',methods=["POST"])
+def input2():
+      title=request.form.get("title")
+      season=request.form.get("season")
+      star=request.form.get("star")
+      comment=request.form.get("comment")
+      postuser=request.form.get("postuser")
+      print("title=",title)
+      conn=sqlite3.connect('anime.db')
+      cur=conn.cursor()
+      cur.execute("INSERT INTO anime(animename,season,star,comment,postuser) VALUES(?,?,?,?,?)"
+      ,(title,season,star,comment,postuser))
+      conn.commit()
+      conn.close()
+      return ""
+if __name__ =="__main__":
+    app.run(debug=True)
---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME="kensin"
+
+echo "こんにちは $NAME さん"
+echo "今日は $(date) です"
